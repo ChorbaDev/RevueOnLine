97b258b00a998a844ba3714015450b5340e85c47
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2021-09-13 15:35:50 +0200

    Update MySQLClientDAO.java

--- TD1/src/DAO/MySQL/MySQLClientDAO.java
+++ TD1/src/DAO/MySQL/MySQLClientDAO.java
@@ -1,4 +1,33 @@
 package DAO.MySQL;
 
-public class MySQLClientDAO {
+import DAO.ClientDAO;
+import Metier.Client;
+
+import java.util.ArrayList;
+
+public class MySQLClientDAO implements ClientDAO {
+    @Override
+    public ArrayList<Client> getByNomPrenom(String nom, String prenom) {
+        return null;
+    }
+
+    @Override
+    public Client getById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean create(Client object) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Client object) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Client object) {
+        return false;
+    }
 }
