f51b04589e57aa87194932939e6e99aa86772e6d
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2021-09-17 22:18:15 +0200

    Update ListeMemoirePeriodiciteDAO.java

--- TD1/src/ListeMemoire/ListeMemoirePeriodiciteDAO.java
+++ TD1/src/ListeMemoire/ListeMemoirePeriodiciteDAO.java
@@ -5,30 +5,69 @@ import Metier.Periodicite;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListeMemoirePeriodiciteDAO implements PeriodiciteDAO {
+    private static ListeMemoirePeriodiciteDAO instance;
+    private List<Periodicite> donnees;
+
+    private ListeMemoirePeriodiciteDAO() {
+        this.donnees = new ArrayList<>();
+    }
+
+    public static ListeMemoirePeriodiciteDAO getInstance() {
+        if (instance == null)
+            instance = new ListeMemoirePeriodiciteDAO();
+        return instance;
+    }
+
     @Override
     public Periodicite getById(int id) throws SQLException {
-        return null;
+        for (Periodicite p : donnees) {
+            if (p.getCle() == id)
+                return p;
+        }
+        throw new IllegalArgumentException("Aucun objet de ce type ne possède cet identifiant");
     }
 
     @Override
     public boolean create(Periodicite object) throws SQLException {
-        return false;
+        object.setCle(donnees.size() + 1);
+        boolean validAjout = this.donnees.add(object);
+        return validAjout;
     }
 
     @Override
     public boolean update(Periodicite object) throws SQLException {
-        return false;
+        int idx = this.donnees.indexOf(object);
+        if (idx == -1) {
+            throw new IllegalArgumentException("Tentative de modification d'un objet inexistant");
+
+        } else {
+            this.donnees.set(idx, object);
+        }
+        return true;
     }
 
     @Override
     public boolean delete(Periodicite object) throws SQLException {
-        return false;
+        Periodicite supprime;
+        int idx = this.donnees.indexOf(object);
+        if (idx == -1) {
+            throw new IllegalArgumentException("Tentative de suppression d'un objet inexistant");
+        } else {
+            supprime = this.donnees.remove(idx);
+        }
+        return object.equals(supprime);
     }
 
     @Override
     public ArrayList<Periodicite> getByLibelle(String libelle) throws SQLException {
-        return null;
+        ArrayList<Periodicite> listePer = new ArrayList<>();
+        for (Periodicite p : donnees) {
+            if (p.getLibelle() == libelle)
+                listePer.add(p);
+        }
+        return listePer;
     }
 }
