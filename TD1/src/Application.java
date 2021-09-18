dbb9b7d35d2ebcea83a19e01a20d7dd199561436
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2021-09-18 17:22:55 +0200

    Revert "Merge branch 'dev-yg2'"
    This reverts commit f056d9167022ebff104f75f5027176851345e813, reversing
    changes made to 6e37f08d2939d0d343f8f7be3ff46d0d46910bb6.

--- TD1/src/Application.java
+++ TD1/src/Application.java
@@ -8,7 +8,6 @@ import Metier.Abonnement;
 import Metier.Revue;
 
 public class Application {
-    static DAOFactory daos=null;
 
 	public static void main(String[] args) throws SQLException {
 		String reponse;
@@ -50,7 +49,6 @@ public class Application {
 		do {
 			choixOperation = Integer.parseInt(sc.nextLine());
 		} while (choixOperation < 1 || choixOperation > 4);
-		daos=choixPersistance(sc);
 		switch (choixOperation) {
 		case 1:
 			createRevue(sc);
@@ -147,30 +145,7 @@ public class Application {
 			break;
 		}
 
-    private static void manipPeriodicite(Scanner sc) throws SQLException {
-        afficheCRUD();
-        int choixOperation;
-        do {
-            choixOperation = sc.nextInt();
-        } while (choixOperation < 1 || choixOperation > 4);
-        daos=choixPersistance(sc);
-        switch (choixOperation) {
-            case 1:
-                ;
-                break;
-            case 2:
-                requestPeriodicite(sc);
-                break;
-            case 3:
-                ;
-                break;
-            case 4:
-                ;
-                break;
-            default:
-                break;
-        }
-    }
+	}
 
 	private static void reqTitreRevue(Scanner sc) throws SQLException {
 		String titre = sc.nextLine();
@@ -190,30 +165,9 @@ public class Application {
 		afficheCRUD();
 	}
 
-    private static void manipClient(Scanner sc) throws SQLException {
-        afficheCRUD();
-        int choixOperation;
-        do {
-            choixOperation = sc.nextInt();
-        } while (choixOperation < 1 || choixOperation > 4);
-        daos=choixPersistance(sc);
-        switch (choixOperation) {
-            case 1:
-                ;
-                break;
-            case 2:
-                requestClient(sc);
-                break;
-            case 3:
-                ;
-                break;
-            case 4:
-                ;
-                break;
-            default:
-                break;
-        }
-    }
+	private static void manipClient(Scanner sc) {
+		afficheCRUD();
+	}
 
 	private static void manipAbonnement(Scanner sc) throws SQLException {
 		afficheCRUD();
@@ -309,88 +263,6 @@ public class Application {
 		Abonnement abonnement = new Abonnement(date_deb, date_fin, id_r, id_c);
 		DAOFactory.getDAOFactory(Persistance.ListeMemoire).getAbonnementDAO().create(abonnement);
 	}
-    }
-
-    private static DAOFactory choixPersistance(Scanner sc) {
-        System.out.println("Choix de la solution de persistance:\n"
-                + "1-MySQL\n"
-                + "2-ListeMemoire\n");
-        int choix;
-        do {
-            choix = sc.nextInt();
-        } while (choix < 1 || choix > 2);
-        switch (choix) {
-            case 1:
-                daos=DAOFactory.getDAOFactory(Persistance.MYSQL);
-                break;
-            case 2:
-                daos=DAOFactory.getDAOFactory(Persistance.ListeMemoire);
-                break;
-            default:
-                break;
-        }
-        return daos;
-    }
-}
-/*  // TODO Auto-generated method stub
-Scanner scanner = new Scanner(System.in);
-Client c = new Client("gho", "you", "5", "fekoe", "5700", "yutz", "fr");
-Periodicite p = new Periodicite("bonjour");
-Revue r = new Revue("Sa majesté le roi des mouches", " le roi de mouches", 10.95, "beau", 5);
-Abonnement a = new Abonnement("10/09/21", "11/10/21", 12, 1);
-System.out.println("que souhaitez vous faire ? : 1- ajouter, 2-modifier, 3-supprimer");
-int choixtravail = scanner.nextInt();
-System.out.println("sur quelle table souhaitez vous travailler ? : 1- Periodicité, 2-Metier.Client, 3-Metier.Revue, 4-Metier.Abonnement");
-int choixtable = scanner.nextInt();
-switch (choixtable) {
-    case 1:
-        switch (choixtravail) {
-            case 1:
-                p.addPeriodicite();
-                break;
-            case 2:
-                p.updatePeriodicite("aurevoir");
-                break;
-            case 3:
-                p.deletePeriodicite();
-                break;
-        }
-    case 2:
-        switch (choixtravail) {
-            case 1:
-                c.addClient();
-                break;
-            case 2:
-                c.updateClient("gholem", "youss", "12", "the voice", "57500", "metz", "marsa");
-                break;
-            case 3:
-                c.deleteClient();
-                break;
-        }
-    case 3:
-        switch (choixtravail) {
-            case 1:
-                r.addRevue();
-                break;
-            case 2:
-                r.updateRevue("Sa majesté le roi des mouches", " le roi de mouches", 10.95, "beau", 5);
-                break;
-            case 3:
-                r.deleteRevue();
-                break;
-        }
-    case 4:
-        switch (choixtravail) {
-            case 1:
-                a.addAbonnement();
-                break;
-            case 2:
-                a.updateAbonnement("10/09/21", "11/10/22", 12, 1);
-                break;
-            case 3:
-                a.deleteAbonnement();
-                break;
-        }
 
 	private static void reqClientAbonnement(Scanner sc) throws SQLException {
 		int id_c = Integer.parseInt(sc.nextLine());
