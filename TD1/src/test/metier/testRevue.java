05dbce3cfe793e23819e7b979d18657aef91c801
The Morrigan <53189749+OmarEll@users.noreply.github.com>
2021-09-27 16:44:57 +0200

    test Revue + Abonnement Equals and HashCode

---
+++ TD1/src/test/metier/testRevue.java
@@ -0,0 +1,30 @@
+package test.metier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import metier.Revue;
+
+public class testRevue {
+	private Revue r1;
+	private Revue r2;
+
+	@Before
+	public void setUp() {
+		r1 = new Revue(1, "test", "test", 1.0, "test", 1);
+		r2 = new Revue(1, "test", "test", 1.0, "test", 1);
+	}
+
+	@Test
+	public void testVerifEquals() {
+		assertTrue(r1.equals(r2));
+	}
+
+	@Test
+	public void testHashCode() {
+		assertEquals(r1.hashCode(), r2.hashCode());
+	}
+}
