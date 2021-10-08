31d1b6ece6e5232f362c5c475fa7e56f569f0f65
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-10-09 00:19:54 +0200

    Update ProcessAdresse -> normalizeAdresse, normalizePays, normalizeNomDeVoie

--- TD1/src/process/ProcessAdresse.java
+++ TD1/src/process/ProcessAdresse.java
@@ -4,29 +4,55 @@ import metier.Adresse;
 
 public class ProcessAdresse {
 
-	public Adresse normalizeAdresse(Adresse adresse) {
+	public void normalizeAdresse(Adresse adresse) {
 		String paysNormalized = normalizePays(adresse.getPays());
 		String villeNormalized = normalizeVille(adresse.getVille());
 		String codePostalNormalized = normalizeCodePostal(adresse.getCode_postal());
 		String nomDeVoieNormalized = normalizeNomDeVoie(adresse.getVoie());
 		String numVoieNormalized = normalizeNumDeVoie(adresse.getNo_rue());
-		Adresse adresseNormalized = new Adresse(numVoieNormalized, nomDeVoieNormalized, codePostalNormalized,
-				villeNormalized, paysNormalized);
-		return adresseNormalized;
+		adresse.setPays(paysNormalized);
+		adresse.setVille(villeNormalized);
+		adresse.setCode_postal(codePostalNormalized);
+		adresse.setVoie(nomDeVoieNormalized);
+		adresse.setNo_rue(numVoieNormalized);
 	}
 
 	public String normalizePays(String pays) {
-		return null;
+		if(pays.equalsIgnoreCase("letzebuerg")){
+			return "Luxembourg";
+		}else if(pays.equalsIgnoreCase("belgium"))
+			return "Belgique";
+		else if (pays.equalsIgnoreCase("switzerland")||pays.equalsIgnoreCase("schweiz"))
+			return "Suisse";
+		else return pays.substring(0,1).toUpperCase()+pays.substring(1).toLowerCase().trim();
 	}
 
 	public String normalizeNomDeVoie(String nomDeVoie) {
-		return null;
+		nomDeVoie=nomDeVoie.trim();
+		String[] boulevardAbv={"boul","boul.","bd"};
+		String[] faubourgAbv={"faub.","fb"};
+
+		if (nomDeVoie.contains("av"))
+			return nomDeVoie.replace("av","avenue");
+		else if (nomDeVoie.contains("pl"))
+			return nomDeVoie.replace("pl","place");
+		for(String elt: boulevardAbv){
+			if (nomDeVoie.contains(elt))
+				return nomDeVoie.replace(elt,"boulevard");
+		}
+		for(String elt:faubourgAbv){
+			if (nomDeVoie.contains(elt))
+				return nomDeVoie.replace(elt,"faubourg");
+		}
+
+
+		return nomDeVoie;
 	}
 
 	private String reforme(String str, String[] tab) {
 		for (String elt : tab) {
 			String ch = elt;
-			if (str.contains(str))
+			if (str.contains(str))//j'ai une question
 				ch = '-' + elt.trim() + '-';
 			else if (str.contains(elt.substring(1)))
 				ch = elt.trim() + '-';
@@ -40,7 +66,7 @@ public class ProcessAdresse {
 	public String normalizeVille(String ville) {
 		ville = ville.substring(0, 1).toUpperCase() + ville.substring(1);
 		//
-		String[] preposition = { " lès ", " le ", " sous ", " à ", " aux " };
+		String[] preposition = { " lï¿½s ", " le ", " sous ", " ï¿½ ", " aux " };
 		ville = reforme(ville, preposition);
 		//
 		String[] saints = { " St ", " Saint " };
@@ -51,7 +77,7 @@ public class ProcessAdresse {
 	public String normalizeCodePostal(String codePostal) {
 		codePostal = codePostal.replaceAll("[^\\d]", "");
 		while (codePostal.length() < 5)
-			codePostal = '0' + codePostal;
+			codePostal = '0' + codePostal;//autre question
 		return codePostal;
 	}
 
