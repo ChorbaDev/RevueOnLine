ad5bc4e7543bdaeafce1ff2fddc2b14dfa8b1f75
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-10-09 09:21:38 +0200

    Update ProcessTest -> NormalisationCodePostalTest passed,NormalisationVilleTest failed, NormalisationNumDeVoieTest passed

--- TD1/src/test/process/processTest.java
+++ TD1/src/test/process/processTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import metier.Adresse;
 import process.ProcessAdresse;
 
+import static org.junit.Assert.assertEquals;
+
 public class processTest {
 	private ProcessAdresse pr;
 	private Adresse ad;
@@ -32,16 +34,23 @@ public class processTest {
 
 	@Test
 	public void NormalisationVilleTest() {
+		String ville= ad.getVille();
+		ville=pr.normalizeVille(ville);
+		assertEquals("Saint-Julien-aux-Metz-sur",ville);
 
 	}
 
 	@Test
 	public void NormalisationCodePostalTest() {
-
+		String cp=ad.getCode_postal();
+		cp=pr.normalizeCodePostal(cp);
+		assertEquals("05400",cp);
 	}
 
 	@Test
 	public void NormalisationNumDeVoieTest() {
-
+		String numVoie=ad.getNo_rue();
+		numVoie=pr.normalizeNumDeVoie(numVoie);
+		assertEquals("6, ",numVoie);
 	}
 }
