5f47b8e204c65c73476ce170c7f1b9aefecf1ef8
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2021-10-28 16:57:40 +0200

    reformat code vue/dialog

--- TD1/src/vue/dialogFiles/DialogMAJ.java
+++ TD1/src/vue/dialogFiles/DialogMAJ.java
@@ -24,45 +24,41 @@ import java.net.URL;
 import java.sql.SQLException;
 
 public class DialogMAJ<C> extends Stage {
-    public DialogMAJ(AnchorPane anchor, DaoFactory dao, TableView tab,URL fxmlURL) throws IOException, SQLException, ClassNotFoundException {
+    public DialogMAJ(AnchorPane anchor, DaoFactory dao, TableView tab, URL fxmlURL) throws IOException, SQLException, ClassNotFoundException {
         final FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
-        final Parent node = (Parent)fxmlLoader.load();
+        final Parent node = (Parent) fxmlLoader.load();
         Scene scene = new Scene(node);
         this.initModality(Modality.APPLICATION_MODAL);
         this.initStyle(StageStyle.UNDECORATED);
         C cast = fxmlLoader.getController();
-        if(cast instanceof CtrlAjoutClient){
-            CtrlAjoutClient controleur=(CtrlAjoutClient) cast;
-            controleur.setVue(this,anchor,dao,tab);
-        }
-        else if(cast instanceof CtrlModifClient){
-            CtrlModifClient controleur=(CtrlModifClient) cast;
-            controleur.setVue(this,anchor,dao,tab);
-        }
-        else if(cast instanceof CtrlModifRevue){
-            CtrlModifRevue controleur=(CtrlModifRevue) cast;
-            controleur.setVue(this,anchor,dao,tab);
-        }
-        else if (cast instanceof CtrlAjoutRevue){
-            CtrlAjoutRevue controleur=(CtrlAjoutRevue) cast;
-            controleur.setVue(this,anchor,dao,tab);
-        }else if(cast instanceof CtrlAjoutPeriodicite){
-            CtrlAjoutPeriodicite controleur=(CtrlAjoutPeriodicite) cast;
-            controleur.setVue(this,anchor,dao,tab);
-        }else if(cast instanceof CtrlModifPeriodicite){
-            CtrlModifPeriodicite controleur=(CtrlModifPeriodicite) cast;
-            controleur.setVue(this,anchor,dao,tab);
-        }
-        else if(cast instanceof CtrlAjoutAbonnement){
-            CtrlAjoutAbonnement contoleur=(CtrlAjoutAbonnement) cast;
-            contoleur.setVue(this,anchor,dao,tab);
-        }else{
-            CtrlModifAbonnement contoleur=(CtrlModifAbonnement) cast;
-            contoleur.setVue(this,anchor,dao,tab);
+        if (cast instanceof CtrlAjoutClient) {
+            CtrlAjoutClient controleur = (CtrlAjoutClient) cast;
+            controleur.setVue(this, anchor, dao, tab);
+        } else if (cast instanceof CtrlModifClient) {
+            CtrlModifClient controleur = (CtrlModifClient) cast;
+            controleur.setVue(this, anchor, dao, tab);
+        } else if (cast instanceof CtrlModifRevue) {
+            CtrlModifRevue controleur = (CtrlModifRevue) cast;
+            controleur.setVue(this, anchor, dao, tab);
+        } else if (cast instanceof CtrlAjoutRevue) {
+            CtrlAjoutRevue controleur = (CtrlAjoutRevue) cast;
+            controleur.setVue(this, anchor, dao, tab);
+        } else if (cast instanceof CtrlAjoutPeriodicite) {
+            CtrlAjoutPeriodicite controleur = (CtrlAjoutPeriodicite) cast;
+            controleur.setVue(this, anchor, dao, tab);
+        } else if (cast instanceof CtrlModifPeriodicite) {
+            CtrlModifPeriodicite controleur = (CtrlModifPeriodicite) cast;
+            controleur.setVue(this, anchor, dao, tab);
+        } else if (cast instanceof CtrlAjoutAbonnement) {
+            CtrlAjoutAbonnement contoleur = (CtrlAjoutAbonnement) cast;
+            contoleur.setVue(this, anchor, dao, tab);
+        } else {
+            CtrlModifAbonnement contoleur = (CtrlModifAbonnement) cast;
+            contoleur.setVue(this, anchor, dao, tab);
         }
 
 
-        CommunStaticMethods.blurStage(anchor,5,5,5);
+        CommunStaticMethods.blurStage(anchor, 5, 5, 5);
         this.setScene(scene);
         this.show();
     }
